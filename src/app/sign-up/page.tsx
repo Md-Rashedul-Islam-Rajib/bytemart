9f45897004aab072548677596db3117861495669
Md Rashedul Islam <rajib5570@gmail.com>
2024-09-25 02:56:03 +0600

    react-hook-form and zod validation library installed

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,6 +1,21 @@
+import { useRouter } from 'next/router';
 import React from 'react'
 
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+    Input,
+    Button,
+  } from "shadcn/ui";
+
+  const signUpSchema = z
+
 const SignUp = () => {
+    const router = useRouter();
   return (
     <div>
       
