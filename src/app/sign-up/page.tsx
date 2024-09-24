24dd84e2b39b41f51ca517304d099ef931dbc0f7
Md Rashedul Islam <rajib5570@gmail.com>
2024-09-25 03:39:56 +0600

    sign up form stucture added

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,26 +1,70 @@
-import { useRouter } from 'next/router';
-import React from 'react'
-
+import { useRouter } from "next/router";
+import React from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-    Input,
-    Button,
-  } from "shadcn/ui";
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  Input,
+  Button,
+} from "shadcn/ui";
+import { useForm } from "react-hook-form";
 
-  const signUpSchema = z
+const signUpSchema = z.object({
+  name: z
+    .string()
+    .min(3, {
+      required_error: "Name is required",
+      invalid_type_error: "Name must be 3 or more characters long",
+    }),
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters long" }),
+  image: z.any(),
+});
+
+type SignUpFormValues = z.infer<typeof signUpSchema>;
 
 const SignUp = () => {
-    const router = useRouter();
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const router = useRouter();
 
-export default SignUp
+    const form = useForm<SignUpFormValues>({
+        resolver: zodResolver(signUpSchema),
+        defaultValues: {
+            name: "",
+            email: "",
+            password: "",
+            image: null,
+        },
+    });
+
+
+    const onSubmit = (data: SignUpFormValues) => {
+        console.log(data);
+        // router.push("/");
+    }
+
+  return 
+  <div className="flex justify-center items-center h-screen">
+
+    <Form {...form}>
+        <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-full max-w-md p-6 bg-white shadow-md rounded"
+        >
+            <h1 className="text-2xl font-bold mb-6">Sign Up</h1>
+
+        </form>
+    </Form>
+
+
+
+  </div>;
+};
+
+export default SignUp;
