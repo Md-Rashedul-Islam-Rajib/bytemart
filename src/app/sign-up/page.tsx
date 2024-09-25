391ffd119aa011e3c23c3ebb89c50d5f6118c459
Md Rashedul Islam <rajib5570@gmail.com>
2024-09-25 21:52:00 +0600

    necessary function added for image hosting

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -40,6 +40,21 @@ const SignUp = () => {
     },
   });
 
+
+  const uploadImage = async (file : File) => {
+    const formData = new FormData();
+    formData.append("file",file);
+    formData.append("upload_preset", "pzk6cmri");
+    formData.append("cloud_name", "dbe3ewhey");
+
+    const res = await fetch("https://api.cloudinary.com/v1_1/dbe3ewhey/image/upload", {
+      method: "POST",
+      body: formData,
+    });
+    const data = await res.json();
+    return data.secure_url;
+  }
+
   const onSubmit = (data: SignUpFormValues) => {
     console.log(data);
     // router.push("/");
